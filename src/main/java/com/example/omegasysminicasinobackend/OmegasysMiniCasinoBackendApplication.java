80b4e9cf32e2678062228b676d44594f206b1651
danizhou <zzjdan@gmail.com>
2025-03-26 17:06:22 -0700

    Initial commit

---
+++ src/main/java/com/example/omegasysminicasinobackend/OmegasysMiniCasinoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.omegasysminicasinobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmegasysMiniCasinoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmegasysMiniCasinoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/omegasysminicasinobackend/OmegasysMiniCasinoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.omegasysminicasinobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmegasysMiniCasinoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
